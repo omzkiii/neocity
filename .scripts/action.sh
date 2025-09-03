91ecb3b0b6f63bde2ef1f0807316fb5be6f83db3
omzkiii <geogeomarsantos@gmail.com>
2025-09-03 18:37:40 +0800

    test secret output

--- .scripts/action.sh
+++ .scripts/action.sh
@@ -1,1 +1,2 @@
-curl -u "omzkiii:$1" -F "index.html=@index.html" "https://neocities.org/api/upload"
+# curl -u "omzkiii:$1" -F "index.html=@index.html" "https://neocities.org/api/upload"
+echo "test: $1"
