a75ddfd2da928af8ff0562f3ad55ceb4b1fe284a
omzkiii <geogeomarsantos@gmail.com>
2025-09-04 01:55:58 +0800

    add more colors, add js scripts

---
+++ city/scripts/index.js
@@ -0,0 +1,5 @@
+const particle = document.createElement("div");
+particle.textContent = "✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦";
+particle.id = "1";
+particle.className = "particle";
+document.body.appendChild(particle);
