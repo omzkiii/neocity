da893a11568dfc240a606e6d0fc5e1b813b63c02
omzkiii <geogeomarsantos@gmail.com>
2025-09-04 02:23:02 +0800

    add stars

--- city/scripts/index.js
+++ city/scripts/index.js
@@ -1,5 +1,14 @@
-const particle = document.createElement("div");
-particle.textContent = "✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦";
-particle.id = "1";
-particle.className = "particle";
-document.body.appendChild(particle);
+for (let i = 0; i < 70; i++) {
+  const x_pos = Math.floor(Math.random() * 100);
+  const y_pos = Math.floor(Math.random() * 100);
+  const particle = document.createElement("div");
+  particle.textContent = "✦";
+  // particle.textContent = y_pos;
+  particle.id = `${i}`;
+  particle.className = "particle";
+  particle.style.position = "absolute";
+  particle.style.left = `${x_pos}vw`;
+  particle.style.bottom = `${y_pos}vh`;
+  particle.style.zIndex = "-1";
+  document.body.appendChild(particle);
+}
