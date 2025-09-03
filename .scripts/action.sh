ffbb1c9e90bda998943d67267c29328c5578bb82
omzkiii <geogeomarsantos@gmail.com>
2025-09-03 19:01:25 +0800

    fix trail bug

--- .scripts/action.sh
+++ .scripts/action.sh
@@ -1,3 +1,3 @@
 for f in $(find city -type f); do
-    curl -u "omzki:$1" -F "${f#/city}=@${f#/city}" "https://neocities.org/api/upload"
+    curl -u "omzki:$1" -F "${f#city/}=@${f#city/}" "https://neocities.org/api/upload"
 done
